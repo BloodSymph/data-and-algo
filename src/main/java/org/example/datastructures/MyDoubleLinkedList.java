be537a807a77f622479e41c963bcc9c9f37db7c5
BloodSymph <rubakharoma@gmail.com>
2024-06-15 11:00:46 +0300

    learning data and algo

--- src/main/java/org/example/datastructures/MyDoubleLinkedList.java
+++ src/main/java/org/example/datastructures/MyDoubleLinkedList.java
@@ -1,23 +1,26 @@
 package org.example.datastructures;
-
+//Реализация двосвязного списка
 public final class MyDoubleLinkedList<T> {
-
+    //Голова списка
     private Node<T> head;
-
+    //Хвост списка
     private Node<T> tail;
-
+    //Конструктор
     public MyDoubleLinkedList() {
         this.head = null;
         this.tail = null;
     }
 
+    //Статический клас узла
     private static class Node<T> {
+        //Значение
         private T value;
-
+        //Ссылка на следующую ноду
         private Node<T> next;
-
+        //Ссылка на придыдущую ноду
         private Node<T> prev;
 
+        //Конструктор
         public Node(T value) {
             this.value = value;
             this.next = null;
@@ -25,61 +28,96 @@ public final class MyDoubleLinkedList<T> {
         }
     }
 
+    //Добавление нового значения
     public void add(T value) {
+        //Создается новый узел и помещаеться передаваемое значение
         Node<T> newNode = new Node<>(value);
 
+        //Если значение головного узла null
         if (head == null) {
+            //Присваиваем ему значение нового узла
             head = newNode;
         } else {
+            //Последующая нода будет являться ссылкой на хвост
             tail.next = newNode;
+            //Придыдущая нода будет являться ссылкой на хвост
             newNode.prev = tail;
         }
+        //Хвост - новая нода
         tail = newNode;
     }
 
-
+    //Реверсия списка
     public void reverse() {
+        //Присваиваем текущей ноде голову
         Node<T> currentNode = head;
+        //Создаём мнимый узел
         Node<T> temp;
 
+        //В цикле: если ссылка на текущую ноду не null
         while (currentNode != null) {
+            //Мнимому узлу присваиваем ссылку на следующий узел
             temp = currentNode.next;
+            //Присваиваем ссылке на последующий узел ссылку на предыдущий
             currentNode.next = currentNode.prev;
+            //Если мнимый узел не null
             if (temp != null) {
+                //Ссылке на предыдущий узел присваиваем временный мнимый узел
                 currentNode.prev = temp;
+                //Текущему узлу присваиваем мнимый временный узел
                 currentNode = temp;
             } else {
+                //Если ссылки на предыдущий узел нету присваиваем null
                 currentNode.prev = null;
+                //Если текущего узла не существует присваиваем null
                 currentNode = null;
             }
         }
+        //В нимый временный узел записываем голову
         temp = head;
+        //В голову записываем хвост
         head = tail;
+        //В хвост значение мнимого узла
         tail = temp;
     }
 
+    //Удаление значения из списка
     public void remove(T value) {
+        //Присваиваем текущей ноде голову
         Node<T> currentNode = head;
+        //Придыдущей ноде значение null
         Node<T> prevNode = null;
 
+        //В цикле: если ссылка на последующие ноды не null
         while (currentNode.next != null) {
+            //Проверяем текущое значение с передаваемым
             if (currentNode.value == value) {
+                //если текущая нода является головой
                 if (currentNode == head){
+                    //Присваиваем голове ссылку на последующую ноду
                     head = currentNode.next;
                 } else {
+                    // Или присваиваем ссылке на придыдущее ноды на ссылку последующих
                     prevNode.next = currentNode.next;
                 }
             }
+            //присваиваем придыдущей ноде текущую ноду
             prevNode = currentNode;
+            //Присваиваем текущей ноде ссылку на следующие ноды
             currentNode = currentNode.next;
         }
     }
 
+    //Распечатать лист
     public void printList() {
+        //Текущая нода равна голове
         Node<T> currentNode = head;
 
+        //Запускаем цикл: пока ссылка на другую ноду не null
         while (currentNode != null) {
+            //Выводим на экран значение текущей ноды
             System.out.println(currentNode.value);
+            //Текущей ноде присваиваем значение последующей
             currentNode = currentNode.next;
         }
         System.out.println();
--- src/main/java/org/example/datastructures/MyLinkedList.java
+++ src/main/java/org/example/datastructures/MyLinkedList.java
@@ -1,63 +1,91 @@
 package org.example.datastructures;
-
+//Реализация односвязного списка
 public final class MyLinkedList<T> {
-
+    //Головной конец списка
     private Node<T> head;
 
+    //Конструктор инициализации
     public MyLinkedList() {
         this.head = null;
     }
 
+    //Статіческий клас которий представляет узел списка
     private static class Node<T> {
+        //Ссылка на последующий узел
         private Node<T> next;
+        //Значение передаваемое в узел
         private T value;
 
+        //Конструктор инициализации
         public Node(T value) {
             this.next = null;
             this.value = value;
         }
     }
 
+    //Вставка значения
     public void add(T value) {
+        //Создается новый узел и помещаеться передаваемое значение
         Node<T> newNode = new Node<>(value);
+        //Текущий узел является головой списка
         Node<T> currentNode = head;
 
+        //Если значение головного узла null
         if (head == null) {
+            //Присваиваем ему значение нового узла
             head = newNode;
         } else {
+            //Запускаем цикл: если текущий узел с сылкой на последующие не содержит null
             while (currentNode.next != null) {
+                //Присваиваем текущему узлу ссылку на последующую ноду
                 currentNode = currentNode.next;
             }
+            //Ссылаемся на новый узел
             currentNode.next = newNode;
         }
     }
 
+    //Удаление значения из списка
     public void remove(T value) {
+        //Присваиваем текущей ноде голову
         Node<T> currentNode = head;
+        //Придыдущей ноде значение null
         Node<T> prevNode = null;
 
+        //В цикле: если ссылка на последующие ноды не null
         while (currentNode.next != null) {
+            //Проверяем текущое значение с передаваемым
             if (currentNode.value == value) {
+                //если текущая нода является головой
                 if (currentNode == head){
+                    //Присваиваем голове ссылку на последующую ноду
                     head = currentNode.next;
                 } else {
+                    // Или присваиваем ссылке на придыдущее ноды на ссылку последующих
                     prevNode.next = currentNode.next;
                 }
             }
+            //присваиваем придыдущей ноде текущую ноду
             prevNode = currentNode;
+            //Присваиваем текущей ноде ссылку на следующие ноды
             currentNode = currentNode.next;
         }
     }
 
+    //Распечатать лист
     public void print() {
+        //Текущая нода равна голове
         Node<T> currentNode = head;
-
+        //Если текущая нода не null
         if (head != null) {
+            //Выводим значение головы
             System.out.println(head.value);
         }
-
+        //Запускаем цикл: пока ссылка на другую ноду не null
         while (currentNode.next != null) {
+            //Текущей ноде присваиваем значение последующей
             currentNode = currentNode.next;
+            //Выводим на экран значение текущей ноды
             System.out.println(currentNode.value);
         }
     }
