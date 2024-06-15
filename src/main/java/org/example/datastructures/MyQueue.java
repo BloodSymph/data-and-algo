e5038dda9866a0a792473a5f44e89c169132079b
BloodSymph <rubakharoma@gmail.com>
2024-06-15 11:20:38 +0300

    learning data and algo

--- src/main/java/org/example/datastructures/MyQueue.java
+++ src/main/java/org/example/datastructures/MyQueue.java
@@ -4,14 +4,14 @@ import java.net.StandardSocketOptions;
 import java.util.Objects;
 //Реализация очереди FIFO
 public final class MyQueue<T> {
-
+    //Елементы
     private T[] elements;
-
+    //Начало очереди
     private int front;
 
     //Колличевство елементов очереди
     private int capacity;
-
+    //Конец очереди
     private int back;
 
     //Инициализация очереди
@@ -22,45 +22,60 @@ public final class MyQueue<T> {
         this.elements = null;
     }
 
-    // Очередь пуста
+    // Очередь пусата
+    //True если начало и конец равны -1
     public boolean isEmpty() {
         return front == -1 && back == -1;
     }
 
     // Очередь переполнена
+    //True если конец очереди равен колл єлементов - 1;
     public boolean isFull() {
-        return back == capacity -1;
+        return back == capacity - 1;
     }
 
     //Добавление єлемента в очередь
     public void enqueue(T element) {
+        //Если очередь полна выбрасываем исключение
         if (isFull()) {
             throw new  RuntimeException("Queue is full!");
+            //Если начало и конец равны -1
         } else if (front == -1 && back == -1) {
+            //Перезаписываем начало и конец к значению 0
             front = back = 0;
+            //Добавляем в конец очереди первый элемент
             elements[back] = element;
         } else {
+            //Добавляем элемент инкрементируя в постпрефиксной форме конец очереди
             elements[back++] = element;
         }
     }
 
     //Извлечение єлементов из очереди
     public T dequeue() {
+        //Если очередь пуста выбрасываем исключение
         if (isEmpty()) {
             throw new RuntimeException("Queue is empty!");
+            //Если конец и начало очереди равны
         } else if (front == back) {
+            //Присваиваем им -1
             front = back = -1;
         }
+        //Возвращаем элемент инкрементируя начало в постфиксной форме
         return elements[front++];
     }
 
     //Отображение очереди
     public void display() {
+        //Создаёмм числовую переменную из начением 0
         int i = 0;
+        //Если очередь пуста выбрасываем исключение
         if (isEmpty()) {
             throw new RuntimeException("Queue is empty!");
         } else {
+            //Перебераем в цикле где i = front, back
             for (i = front; i < back; i++) {
+                //Выводим по индексу все элементы очереди
                 System.out.println(elements[i]);
             }
         }
@@ -68,6 +83,7 @@ public final class MyQueue<T> {
 
     // Взять один єлемент из очереди
     public void peek() {
+        //Берём один элемент из начала очереди
         System.out.println("Front value: " + elements[front]);
     }
 
--- src/main/java/org/example/datastructures/MyStack.java
+++ src/main/java/org/example/datastructures/MyStack.java
@@ -3,9 +3,9 @@ package org.example.datastructures;
 import java.util.*;
 //Работает по принципу LIFO
 public final class MyStack<T> {
-
+    //Вершина стека
     private int stackTop;
-
+    //Елементы
     private T[] elements;
 
 
@@ -16,21 +16,25 @@ public final class MyStack<T> {
     }
 
     //Размер стека
+    //Просто возвращаем длину єлемента
     public synchronized int size() {
         return this.elements.length;
     }
 
     //Провека пустой ли стек
+    //Возвращаем true если вершина стека -1
     public synchronized boolean isEmpty() {
         return this.stackTop == -1;
     }
 
-    //Добавление елементов в конец стека
+    //Добавление елементов в стек
+    //Добавляем єлемент по префиксному инкрименту индекса
     public synchronized void push(T elemant) {
         this.elements[++stackTop] = elemant;
     }
 
     //Выборка элементов из вершины стека
+    //Удаляем элемент из стека по постфиксному декрименту индекса
     public synchronized T pop() {
         if (stackTop == -1) {
             throw new RuntimeException();
