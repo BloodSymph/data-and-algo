f3f472f0a7119cff6d24cf4755543372675d81bc
BloodSymph <rubakharoma@gmail.com>
2024-06-16 11:22:50 +0300

    learning data and algo

--- src/main/java/org/example/datastructures/BinarySearchTree.java
+++ src/main/java/org/example/datastructures/BinarySearchTree.java
@@ -1,41 +1,56 @@
 package org.example.datastructures;
-
+//Реализация бинарного дерева
 public final class BinarySearchTree {
-
+    //Корень дерева
     private Node root;
-
+    //Инициализация корня
     public BinarySearchTree() {
         this.root = null;
     }
 
+    //Рекурсивное создание узла на вход текущий узел и значение
     private Node addNode(Node current, int value) {
+        //Базовый случай если текущий узел null
         if (current == null) {
+            //Возвращаем новый узел и заносим туда значение
             return new Node(value);
         }
-
+        //Если передаваемое значение меньше текущего
         if (value < current.value) {
+            //Рекурсивно его добавляем в левую ветку
             current.left = addNode(current.left, value);
+          //Если передаваемое значение выше текущего
         } else if (value > current.value) {
+            //Рекурсивно добавляем его в правую ветку
             current.right = addNode(current.right, value);
         } else {
+            //Иначе возвращаем текущий узел
             return current;
         }
-
+        //Возвращаем текущий узел
         return current;
     }
 
-
+    //Публичный метод добавления нового узла
     public void add(int value) {
+        //Как новый узел добавляем корневой и передаём значение
         root = addNode(root, value);
     }
 
+    //Рекурсивный метод проверки есть ли узел передаём узел и значение
     private boolean containsNodeRecursive(Node current, int value) {
+        //Если текущий узел null
         if (current == null) {
+            //Возвращаем false
             return false;
         }
+        //Если передаваемое значение равно значению текущего узла
         if (value == current.value) {
+            //Возвращаем true
             return true;
         }
+        //Если переданное значение меньше значения текущего узла то рекурсивно обходим левую ветку
+        //В противном случае правую
         return value < current.value
                 ? containsNodeRecursive(current.left, value)
                 : containsNodeRecursive(current.right, value);
@@ -43,50 +58,79 @@ public final class BinarySearchTree {
 
 
 
-
+    //Публичный метод проверки есть ли узел
     public boolean containsNode(int value) {
+        //Вызываем рекурсивный метод передаём в параметре корневой узел и значение
         return containsNodeRecursive(root, value);
     }
 
+    // Рекурсивная функция проверки максимального значения ноды
     private Node getMax(Node node) {
+        //Если нода равна null возвращаем null;
         if (node == null) return null;
+        //Если правая ветвь равна null возвращаем ноду
         if (node.right == null) return node;
+        //Рекурсивно возвращаем эту функцию передавая правую ветвь
         return getMax(node.right);
     }
 
+    // Рекурсивная функция проверки минимального значения ноды
     private Node getMin(Node node) {
+        //Если нода равна null возвращаем null;
         if (node == null) return null;
-        if (node.left == null) return null;
+        //Если левая ветвь равна null возвращаем ноду
+        if (node.left == null) return node;
+        //Рекурсивно возвращаем эту функцию передавая левую ветвь
         return getMin(node.left);
     }
 
+    //Рекурсивная функция удаления узла передаем ноду и значение
     private Node recursiveDelete(Node node, int value) {
+        //Если нода равна null возвращаем null
         if (node == null) return null;
+        //Если передаваемое значение меньше текущего
         if (value < node.value) {
+            //Рекурсивно удаляем левую ветвь и значение
             node.left = recursiveDelete(node.left, value);
+            //Если передаваемое значение выше текущего
         } else if (value > node.value) {
+            //Рекурсивно удаляем правую ветвь и значение
             node.right = recursiveDelete(node.right, value);
         } else {
+            //Если левая ветвь равнва null или правая ветвь равна null
             if (node.left == null || node.right == null) {
+                //То текущему узлу присваиваем либо ту либо ту ветку
                 node = (node.left == null) ? node.right : node.left;
             } else {
+                //Находим максимальное значение нод в левой ветви
                 Node maxInLeft = getMax(node.left);
+                //Присваиваем текущей ноде максимальное значение ноды левой ветви
                 node.value = maxInLeft.value;
+                //Для правой ветви рекурсивно производим удаление передавая
+                // правую ветвь и максимальное значение ноды
                 node.right = recursiveDelete(node.right, maxInLeft.value);
             }
         }
+        //Возвращаем узел
         return node;
     }
 
+    //Публичный метод удаления
     public void delete(int value) {
+        //В рекурсивную функцию удаления передаем корневой узел и значение
         recursiveDelete(root, value);
     }
 
+    //Статический клас представляющий узел дерева
     private static class Node {
+        //Значение
         private int value;
+        //Левая ветвь
         private Node left;
+        //Правая ветвь
         private Node right;
 
+        //Инициализация узла
         public Node(int value) {
             this.value = value;
             this.left = null;
--- src/main/java/org/example/datastructures/Graph.java
+++ src/main/java/org/example/datastructures/Graph.java
@@ -1,35 +1,52 @@
 package org.example.datastructures;
 
 import java.util.*;
-
+//Реализация графа
 public final class Graph {
+    //Хеш таблица представляющая смежный лист
+    //Состоит из ключа - вершины и масива где будут хранится рёбра графа
     private Map<String, List<String>> adjacentList;
-
+    //Инициализация листа
     public Graph() {
         this.adjacentList = new HashMap<>();
     }
 
+    //Создание новой вершины графа
     public void addVertex(String vertex) {
+        //Если вершины не существует в смежном листе
         if (!adjacentList.containsKey(vertex)) {
+            //Помещаем новую вершину и создаём пустое ребро
             adjacentList.put(vertex, new ArrayList<>());
         }
     }
 
+    //Добавление рёбер передаём две вершины
     public void addEdge(String vertex1, String vertex2) {
+        //Если вершины не существует в смежном листе
         if (!adjacentList.containsKey(vertex1)) {
+            //Добавляем новую вершину
             addVertex(vertex1);
         }
+        //Если второй вершины не существует в смежном листе
         if (!adjacentList.containsKey(vertex2)) {
+            //Добавляем новую
             addVertex(vertex2);
         }
+        //Получаем первую вершину из смежного листа и добавляем вторую в качевстве ребра
         adjacentList.get(vertex1).add(vertex2);
+        //Получаем вторую вершину из смежного листа и добавляем первую в качевстве ребра
         adjacentList.get(vertex2).add(vertex1);
     }
 
+    //Распечатаем граф
     public void printGraph() {
+        //Перебераем каждую вершину в цикле
         for (String vertex : adjacentList.keySet()) {
+            //Выводим их на экран
             System.out.println(vertex + ": ");
+            //Перебераем соседние вершины в цикле
             for (String neighbor : adjacentList.get(vertex)) {
+                //Выводим их на экран
                 System.out.println(neighbor + " ");
             }
             System.out.println();
